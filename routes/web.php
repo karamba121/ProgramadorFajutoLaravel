e3e20b7f48fdcaf9a6973a987c9660a070196afb
matheus santos <matheus.stag@gmail.com>
2019-03-21 16:46:56 -0300

    Adicionado autenticação; Adicionado Banco de dados PostGres

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/post', 'PostController@index')->name('post');
 Route::get('/ferramentas', 'FerramentasController@index')->name('ferramentas');
-Route::get('/login', 'LoginController@index')->name('login');
+
+Auth::routes();
