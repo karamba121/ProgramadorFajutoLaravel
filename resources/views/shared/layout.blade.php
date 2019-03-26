643be61febcd635ed6f470a71c30e7af85c53bbc
matheus santos <matheus.stag@gmail.com>
2019-03-26 17:01:00 -0300

    Adicionado imagens que faltavam.

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="Description" content="Author: Programador Fajuto,
+    Illustrator: Programador Fajuto, Category: Blog">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ $title }}</title>
