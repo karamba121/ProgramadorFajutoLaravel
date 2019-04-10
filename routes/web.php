3c61f15d473dc4b8ec56f56cb07c5a7e17a6e7d6
matheus santos <matheus.stag@gmail.com>
2019-04-10 17:42:29 -0300

    Removido Rotas de autenticação.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/post', 'PostController@index')->name('post');
+Route::get('/login', 'LoginController@index')->name('login');
 Route::get('/ferramentas', 'FerramentasController@index')->name('ferramentas');
-
-Auth::routes();
